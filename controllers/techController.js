7f4e899dd06243284d2b53a1a913ca6c77ddd283
briancanlas <canlasbrian@yahoo.com>
2017-08-28 09:12:14 -0700

    first commit

---
+++ controllers/techController.js
@@ -0,0 +1,15 @@
+angular
+  .module("hubApp")
+  .controller("techController", function($http, $scope, $state, $stateParams){
+
+
+    $http.get('API_KEY').then (function(response) {
+    console.log(response.data.articles);
+    $scope.articles = response.data.articles
+  }),
+  function(error){
+    console.log(error);
+    }
+    
+    
+})
---
+++ services/gifService.js
@@ -0,0 +1,25 @@
+angular
+  .module("hubApp")
+  .service("gifService", function ($http) {
+
+    var currentTestMeme = null;
+
+    this.setCurrentMeme = function(meme) {
+      console.log(meme);
+      currentTestMeme = meme
+      console.log(currentTestMeme)
+    }
+
+    this.getCurrentMeme = function() {
+      return currentTestMeme;
+    }
+
+
+  //   $scope.grabMeme = function(meme) {
+  //   console.log(meme.url)
+  //   $scope.currentMeme.push(meme);
+  //   console.log($scope.currentMeme)
+  // }
+
+
+  });
